15abf7ed4fa1196734fc7520913dca9ebd5089d5
Stephanie Brito <stephaniebritoc2@gmail.com>
2024-09-25 14:27:44 -0300

    retirado comentarios

--- java/exception-handler-example-api/src/main/java/com/example/ExceptionHandlerExampleApiApplication.java
+++ java/exception-handler-example-api/src/main/java/com/example/ExceptionHandlerExampleApiApplication.java
@@ -2,17 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.context.annotation.ComponentScan;
 
-// import springfox.documentation.oas.annotations.EnableOpenApi;
-// import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-// @EnableWebMvc
 @SpringBootApplication
-// @EnableSwagger2
-// @EnableOpenApi
-// @ComponentScan
 public class ExceptionHandlerExampleApiApplication {
 
 	public static void main(String[] args) {
--- java/exception-handler-example-api/src/main/java/com/example/models/Teste.java
+++ java/exception-handler-example-api/src/main/java/com/example/models/Teste.java
@@ -1,5 +1,12 @@
 package com.example.models;
 
-public class Test {
+public class Teste {
     private String teste = "success test";
+
+    public Teste() {
+    }
+
+    public String getTeste() {
+        return teste;
+    }
 }
